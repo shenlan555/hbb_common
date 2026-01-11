6340be12352778f078303e9469ea9d142996e732
shenlan555 <31352778@qq.com>
2026-01-11 15:24:48 +0800

    关闭更新和启用UDP

--- src/config.rs
+++ src/config.rs
@@ -1810,7 +1810,24 @@ pub struct LocalConfig {
 
 impl LocalConfig {
     fn load() -> LocalConfig {
-        Config::load_::<LocalConfig>("_local")
+        let mut config = Config::load_::<LocalConfig>("_local");
+
+            if !config.options.contains_key("enable-check-update") {
+                config.options.insert("enable-check-update".to_string(), "N".to_string());
+                store = true;
+            }
+            
+            let mut store = false;
+    
+            if !config.options.contains_key("enable-udp-punch") {
+                config.options.insert("enable-udp-punch".to_string(), "Y".to_string());
+                store = true;
+            }
+    
+            if store {
+                onfig.store();
+            }
+        config
     }
 
     fn store(&self) {
